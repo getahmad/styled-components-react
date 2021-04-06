3a8de76dd83d2bb1d4bbb2296751620eb71a6a06
Ahmad Danang <ahmad.danang91@gmail.com>
2021-04-06 12:06:58 +0700

    learn styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import styled, { ThemeProvider } from "styled-components";
+
+const Theme = {
+  primary: "red",
+  secondary: "green",
+};
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Head = styled.h1`
+  color: ${(props) => props.theme.secondary};
+`;
+
+const Brand = styled.img`
+  width: 50vh;
+`;
+
+//btn with props
+const Button = styled.button`
+  color: #fff;
+  background: ${(props) => (props.primary ? "blue" : "green")};
+  padding: 0.5rem 1rem;
+  border: none;
+  margin: 0 1rem;
+`;
+
+//extends
+const TomatoButton = styled(Button)`
+  background: tomato;
+`;
+
+//dengan class
+const Wrapper = styled.div`
+  background: yellow;
+  &.test {
+    background: red;
+  }
+`;
+
+const AnotherButton = styled.button`
+  ${(props) => {
+    switch (props.variant) {
+      case "primary":
+        return `background:blue`;
+      case "success":
+        return `background:green`;
+      default:
+        return `background:grey`;
+    }
+  }}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <Container>
+        <Head>styled components</Head>
+        <Brand src={logo} alt="" /> <br />
+        <Button>click</Button>
+        <Button primary>click</Button>
+        <TomatoButton>click</TomatoButton>
+        <Wrapper>biasa</Wrapper>
+        <Wrapper className="test">classname</Wrapper>
+        <AnotherButton variant="primary">primary</AnotherButton>
+        <AnotherButton variant="success">success</AnotherButton>
+        <AnotherButton>gray</AnotherButton>
+      </Container>
+    </ThemeProvider>
   );
 }
 
